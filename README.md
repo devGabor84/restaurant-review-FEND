fedc4726fa9646ad42ebda1fd752e484bacd8b36
Gabor Szephalmi <szephalmig@gmail.com>
2019-01-30 15:07:33 +0000

    service worker added

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Restaurant Review App - FEND
 
-this project is still in development phase.
-
 This is my last project in the Front-End Nanodegree program. It is a Progressive Web Application using Cache API, Service Worker and Google maps API.
+
+Open it on a development server or check it out live:
---
+++ js/sw.js
@@ -0,0 +1,44 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("v1.2").then(cache => {
+      return cache
+        .addAll([
+          "/index.html",
+          "/restaurant.html",
+          "/css/styles.css",
+          "/js/dbhelper.js",
+          "/js/main.js",
+          "/js/restaurant_info.js",
+          "/data/restaurants.json",
+          "/img/1.jpg",
+          "/img/2.jpg",
+          "/img/3.jpg",
+          "/img/4.jpg",
+          "/img/5.jpg",
+          "/img/6.jpg",
+          "/img/7.jpg",
+          "/img/8.jpg",
+          "/img/9.jpg",
+          "/img/10.jpg"
+        ])
+        .catch(error => console.log(error));
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      if (response) {
+        return response;
+      } else {
+        return fetch(e.request).then(response => {
+          caches.open("v1.2").then(cache => {
+            cache.put(e.request, repsonse.clone());
+          });
+          return response;
+        });
+      }
+    })
+  );
+});
